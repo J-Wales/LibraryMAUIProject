04d29656df13947ece864438877a78b89ec56cc5
J-Wales <j.wales@edu.sait.ca>
2024-04-08 10:37:19 -0600

    Basic data viewing functionality

---
+++ sqlHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace LibraryMAUIProject
+{
+    public class sqlHandler
+    {
+
+        public void Read()
+        {
+
+        }
+    }
+}
