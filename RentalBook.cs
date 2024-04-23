847b96a62f740397c35e7eda9410303c12e5ea29
OwenC03 <owen.carson@edu.sait.ca>
2024-04-22 23:32:35 -0600

    comments on pages

--- RentalBook.cs
+++ RentalBook.cs
@@ -9,17 +9,21 @@ namespace LibraryMAUIProject
 {
     public class RentalBook
     {
-        public int rentalID { get; set; }
-        public int bookID { get; set; }
+        // Properties of the RentalBook class
+        public int rentalID { get; set; } // ID of the rental
+        public int bookID { get; set; } // ID of the book
 
+        // Constructor to initialize RentalBook object
         public RentalBook(int rentalID, int bookID)
         {
             this.rentalID = rentalID;
             this.bookID = bookID;
         }
 
+        // Method to add rental book entry to the database table
         public void toTable()
         {
+            // Create a connection string builder to configure the database connection
             MySqlConnectionStringBuilder builder =
                    new MySqlConnectionStringBuilder
                    {
@@ -33,21 +37,26 @@ namespace LibraryMAUIProject
                        Database = "librarydatabase",
                    };
 
+            // Establish a connection to the database
             MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
 
             connection.Open();
 
+            // SQL query to insert rental book entry into the database table
             string sql = $"INSERT INTO rentalbooks (rentalID, bookID) VALUES ({rentalID},{bookID})";
 
+            // Create a MySqlCommand object to execute the SQL query
             MySqlCommand command = new MySqlCommand(sql, connection);
 
-            command.ExecuteNonQuery();
+            command.ExecuteNonQuery(); // Execute the SQL query
 
-            connection.Close();
+            connection.Close(); // Close the database connection
         }
 
+        // Method to remove rental book entry from the database table
         public void Remove()
         {
+            // Create a connection string builder to configure the database connection
             MySqlConnectionStringBuilder builder =
                    new MySqlConnectionStringBuilder
                    {
@@ -61,12 +70,15 @@ namespace LibraryMAUIProject
                        Database = "librarydatabase",
                    };
 
+            // Establish a connection to the database
             MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
 
             connection.Open();
 
+            // SQL query to delete rental book entry from the database table
             string sql = $"DELETE FROM rentalbooks WHERE rentalID = {rentalID} AND bookID = {bookID}";
 
+            // Create a MySqlCommand object to execute the SQL query
             MySqlCommand command = new MySqlCommand(sql, connection);
 
             using (MySqlDataReader reader = command.ExecuteReader())
@@ -74,7 +86,7 @@ namespace LibraryMAUIProject
                 reader.Read();
             }
 
-            connection.Close();
+            connection.Close(); // Close the database connection
 
         }
     }
